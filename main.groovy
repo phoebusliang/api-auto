ba2f9ad7823f6004af752ee11938e271c77731e3
lpy <liangzhen@liangzhens-MacBook-Pro.local>
2023-03-28 17:03:33 +0800

    Zhen LIANG: Add the file testfile.txt

--- main.groovy
+++ main.groovy
@@ -1,1 +1,13 @@
+def cleanUp(folder, file) {
+    sh '''
+        if [ -d folder ]; then
+            rm -rf folder
+        fi
 
+        if [ -e file]; then
+            rm file
+        fi
+    '''
+}
+
+return this
